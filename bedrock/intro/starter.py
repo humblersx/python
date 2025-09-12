25187971151a27f4e14c63028d1af655c7a0df1b
humblersx <humblers@gmail.com>
2025-09-12 15:24:51 -0400

    -

---
+++ bedrock/intro/starter.py
@@ -0,0 +1,19 @@
+import boto3
+import pprint
+bedrock = boto3.client(
+    service_name='bedrock',
+    region_name='us-east-2'
+)
+pp = pprint.PrettyPrinter(depth=4)
+
+def list_foundation_models():
+    models = bedrock.list_foundation_models()
+    for model in models["modelSummaries"]:
+        pp.pprint(model)
+        pp.pprint("------------------------------")
+
+def get_foundation_model(modelIdentifier):
+    model = bedrock.get_foundation_model(modelIdentifier=modelIdentifier)
+    pp.pprint(model)
+
+get_foundation_model('meta.llama3-2-3b-instruct-v1:0')
