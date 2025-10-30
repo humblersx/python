4802307c589847ad9257a2523ec12479f17b98cb
humblersx <humblers@gmail.com>
2025-10-30 15:43:24 -0400

    -

---
+++ Day35/main.py
@@ -0,0 +1,42 @@
+import requests
+from twilio.rest import Client
+import os
+
+api_key = os.environ.get("OWM_API_KEY")
+MY_LAT = 39.961178
+MY_LONG = -82.998795
+
+
+account_sid = "ACf54e34e90fd4a4f6a492b6ef474a4f2b"
+auth_token = os.environ.get("AUTH_TOKEN")
+
+
+
+parameters = {
+    "lat": MY_LAT,
+    "lon": MY_LONG,
+    "appid": api_key,
+    "cnt": 4
+}
+
+response = requests.get("https://api.openweathermap.org/data/2.5/forecast", params=parameters)
+response.raise_for_status()
+
+weather_data = response.json()
+each = 0
+while each < 4:
+    #print(weather_data["list"][each]["weather"][0]["id"])
+    if int(weather_data["list"][each]["weather"][0]["id"]) < 600:
+        client = Client(account_sid, auth_token)
+        message = client.messages.create(
+            body="Bring an umbrella today.",
+            from_="whatsapp:+14155238886",
+            to="whatsapp:+16145585761",
+        )
+        print(message.status)
+        break
+    each += 1
+
+
+
+
