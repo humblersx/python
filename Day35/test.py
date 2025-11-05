e853f928ad54fd8debd126b39f4c9d4a63bf6c4c
humblersx <humblers@gmail.com>
2025-11-05 13:15:35 -0500

    -

--- Day35/test.py
+++ Day35/test.py
@@ -4,7 +4,7 @@ from twilio.rest import Client
 
 
 account_sid = "ACf54e34e90fd4a4f6a492b6ef474a4f2b"
-auth_token = "048afe528e9a4743debe8ab6b24b6494"
+auth_token = "<AUTH_TOKEN>"
 
 client = Client(account_sid, auth_token)
 
